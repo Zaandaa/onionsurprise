e263e95655e32fcfbcbe2a169585ebe75e080360
Zaandaa <adambleclair@gmail.com>
2012-09-11 18:52:58 -0400

    tilting works, has a higher cap

--- Assets/scripts/Field.cs
+++ Assets/scripts/Field.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class Field : MonoBehaviour {
 	
 	// (fore/aft, rotation, left/right)
-	static Vector3 angleCap = new Vector3(20.0f, 0.0f, 20.0f);
+	static float angleCap = 60f;//new Vector3(20.0f, 0.0f, 20.0f);
+	static float angleMid = 180f;
+	static float angleFullCircle = 360f;
 	static Vector3 angleScale = new Vector3(0.5f, 0.0f, 0.5f);
 	
 	void Start () {
@@ -13,13 +15,13 @@ public class Field : MonoBehaviour {
 	}
 	
 	void Update () {
-		Vector3 angleValue;
+		Vector3 angleValue = Vector3.zero;
 		
 		if (Input.gyro.enabled) {
 			Debug.Log("Using gyro: " + Input.gyro.attitude.eulerAngles.ToString());
 			angleValue = Input.gyro.attitude.eulerAngles;
-		//} else if (Input.acceleration != Vector3.zero) {
-		} else if (Input.compass.enabled) {
+		} else if (Input.acceleration != Vector3.zero) {
+//		} else if (Input.compass.enabled) {
 			Debug.Log("Using accel: " + Quaternion.LookRotation(Input.acceleration.normalized).eulerAngles.ToString());
 			if (Input.acceleration.normalized == Vector3.zero) {
 				angleValue = Vector3.zero;
@@ -29,27 +31,36 @@ public class Field : MonoBehaviour {
 				angleValue = Quaternion.LookRotation(Input.acceleration.normalized).eulerAngles;
 			}
 		} else {
-			angleValue = Vector3.zero;
-		
-			if (Input.GetKey(KeyCode.D)) {
-				angleValue.z = -angleCap.z;
-			} else if (Input.GetKey(KeyCode.A)) {
-				angleValue.z = angleCap.z;
-			}
-		
-			if (Input.GetKey(KeyCode.S)) {
-				angleValue.x = -angleCap.x;
-			} else if (Input.GetKey(KeyCode.W)) {
-				angleValue.x = angleCap.x;
-			}
+//			angleValue = Vector3.zero;
+//		
+//			if (Input.GetKey(KeyCode.D)) {
+//				angleValue.z = -angleCap;
+//			} else if (Input.GetKey(KeyCode.A)) {
+//				angleValue.z = angleCap;
+//			}
+//		
+//			if (Input.GetKey(KeyCode.S)) {
+//				angleValue.x = -angleCap;
+//			} else if (Input.GetKey(KeyCode.W)) {
+//				angleValue.x = angleCap;
+//			}
 		}
 		
-		angleValue = Vector3.Scale(angleValue, angleScale);
+		//angleValue = Vector3.Scale(angleValue, angleScale);
 		
-		angleValue.x = Mathf.Clamp(angleValue.x, -angleCap.x, angleCap.x);
-		angleValue.y = Mathf.Clamp(angleValue.y, -angleCap.y, angleCap.y);
-		angleValue.z = Mathf.Clamp(angleValue.z, -angleCap.z, angleCap.z);
+		//allow for negative rotation before clamping
+		if (angleValue.x > angleMid) {
+			angleValue.x -= angleFullCircle;
+		}
+		angleValue.y -= angleMid;
 		
+		//don't exceed angles above angleCap
+		float x = angleValue.x;
+		angleValue.x = Mathf.Clamp(-angleValue.y, -angleCap, angleCap);
+		angleValue.y = 0f;//Mathf.Clamp(angleValue.y, -angleCap, angleCap);
+		angleValue.z = Mathf.Clamp(-x, -angleCap, angleCap);
+		
+		print (angleValue);
 		
 		transform.rotation = Quaternion.Euler(angleValue);
 	}
