b8e71d1b206367405afdf61b7a0fc880a5ad752c
Beth Werbaneth <werbae@rpi.edu>
2012-09-16 18:14:03 -0400

    spinning

---
+++ Assets/scripts/spinning.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+
+public class spinning : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0, Time.deltaTime*15, 0);
+	
+	}
+}
