bcd8cd30c5ad34d9257eec399847a8def1672a76
Colin Neville <nevilc@rpi.edu>
2012-09-18 19:11:57 -0400

    stuff

--- Assets/scripts/menuscript.cs
+++ Assets/scripts/menuscript.cs
@@ -10,10 +10,16 @@ public class menuscript : MonoBehaviour {
 	public Texture2D leader_background;
 	public Texture2D transparent;
 	public GUISkin skin;
+	public Font font;
+	
+	GUIStyle gs = new GUIStyle();
 	
 	// Use this for initialization
 	void Start () {
-		lines = leaderboard.text.Split('\n');	
+		lines = leaderboard.text.Split('\n');
+		Screen.orientation = ScreenOrientation.Portrait;
+		
+		gs.font = font;
 	}
 	
 	// Update is called once per frame
@@ -23,10 +29,14 @@ public class menuscript : MonoBehaviour {
 	
 	void OnGUI(){
 		
+		
+		
 		if(which_menu == "Main"){
 			GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), main_background);
-			if(GUI.Button(new Rect(50, Screen.height/2, Screen.width - 100, Screen.height/10), transparent, ""))
+			if(GUI.Button(new Rect(50, Screen.height/2, Screen.width - 100, Screen.height/10), transparent, "")) {
+				Screen.orientation = ScreenOrientation.Landscape;
 				Application.LoadLevel("newtest");
+			}
 			if(GUI.Button(new Rect(50, Screen.height/2 + Screen.height/8, Screen.width - 100, Screen.height/12), transparent, ""))
 				which_menu = "Leaderboard";
 			if(GUI.Button(new Rect(50, 7*Screen.height/10, Screen.width - 100, Screen.height/10), transparent, ""))
